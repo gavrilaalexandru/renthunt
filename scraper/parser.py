9d13c43941a3b35ddb3cbe78117af9d48760703b
Alexandru-Valentin Gavrila <alexandruvalentingavrila@gmail.com>
2025-03-20 13:24:46 +0200

    created the parser

---
+++ scraper/parser.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup
+
+
+def extract_flat_listings(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    listings = soup.select("a.css-qo0cxu")
+    return listings
+
+
+def extract_flat_details(listings):
+    try:
+        name = listings.select_one("h4.css-1g61gc2").text.strip()
+    except (AttributeError, TypeError):
+        name = "N/A"
+
+    try:
+        price = listings.select_one("p.css-6j1qjp").text.strip()
+    except (AttributeError, TypeError):
+        price = "N/A"
+
+    try:
+        link = listings["href"]
+        if not link.startwith("http"):
+            link = "https://www.olx.ro" + link
+    except (AttributeError, TypeError):
+        link = "N/A"
+
+    return {"name": name, "price": price, "link": link}
+
+
+def extract_pagination_info(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    page_numbers = []
+
+    # li tag
+    pagination_items = soup.select("li[data-testid='pagination-list-item']")
+    for item in pagination_items:
+        aria_label = str(item.get("aria-label", ""))
+        try:
+            page_number = int(aria_label.replace("Page ", "").strip())
+            page_numbers.append(page_number)
+        except ValueError:
+            continue
+
+    # a tag
+    pagination_links = soup.select("a[data-testid^='pagination-link-']")
+    for link in pagination_links:
+        try:
+            page_number = int(link.text.strip())
+            page_numbers.append(page_number)
+        except ValueError:
+            continue
+
+    return max(page_numbers) if page_numbers else 1
