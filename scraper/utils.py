58a7ce33d78157a1269b9381e7db0a643d527699
Alexandru-Valentin Gavrila <alexandruvalentingavrila@gmail.com>
2025-03-20 13:35:09 +0200

    created utils

---
+++ scraper/utils.py
@@ -0,0 +1,30 @@
+import time
+import random
+import os
+import csv
+from datetime import datetime
+
+
+def create_dirs():
+    os.makedirs("data", exist_ok=True)
+
+
+def random_delay(min_seconds=1, max_seconds=3):
+    delay = random.uniform(min_seconds, max_seconds)
+    time.sleep(delay)
+
+
+def save_to_csv(data, filename):
+    if not data:
+        print("No data found")
+        return
+
+    fieldnames = data[0].keys()
+    with open(filename, "w", newline="", encoding="utf-8") as csvfile:
+        w = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        w.writeheader()
+        w.writerows(data)
+
+
+def generate_timestamp():
+    return datetime.now().strftime("%d%m%Y_%H:%M:%S")
