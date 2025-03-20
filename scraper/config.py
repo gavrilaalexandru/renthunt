5758ed065b8b6c3fb12c331087566b888f0451ed
Alexandru-Valentin Gavrila <alexandruvalentingavrila@gmail.com>
2025-03-20 15:05:06 +0200

    changed BASE_URL

--- scraper/config.py
+++ scraper/config.py
@@ -1,4 +1,6 @@
-BASE_URL = "https://www.olx.ro/imobiliare/apartamente-garsoniere-de-vanzare/bucuresti/"
+BASE_URL = (
+    "https://www.olx.ro/imobiliare/apartamente-garsoniere-de-inchiriat/bucuresti/"
+)
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
