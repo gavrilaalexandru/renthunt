a016dd49bbc8b82abdacd115ea5f4455c1bb9fef
Alexandru-Valentin Gavrila <alexandruvalentingavrila@gmail.com>
2025-03-20 12:31:22 +0200

    added the config file

---
+++ config.py
@@ -0,0 +1,9 @@
+BASE_URL = "https://www.olx.ro/imobiliare/apartamente-garsoniere-de-vanzare/bucuresti/"
+
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+}
+
+REQUEST_DELAY = 3
+
+OUTPUT_FILE = "data/apartament_olx.csv"
